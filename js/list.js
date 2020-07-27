9033bdc10b26155606858576ac9fb48e4832a170
GDMeric <66123430+GDMeric@users.noreply.github.com>
2020-07-27 14:12:43 +0200

    Update list.js

--- js/list.js
+++ js/list.js
@@ -20,7 +20,31 @@ const d = {
 			"key": 0
 		},
 		/*=================================================================================*/
-		
+		{
+			"vids": [
+				{
+                                         "user": "Cailloux",
+                                         "link": "https://www.youtube.com/watch?v=UUwwRSZcWNQ",
+                                         "percent": 100,
+                                         "hz": "144hz"
+                                },
+				{
+                                         "user": "AptRock327",
+                                         "link": "https://www.youtube.com/watch?time_continue=1&v=eG3kgDkcguU&feature=emb_logo",
+                                         "percent": 44,
+                                         "hz": "60hz"
+                                },
+			],
+			"name": "Last",
+			"author": "GDMeric",
+			"more": "none",
+			"id": 60,
+			"pass": "Free To Copy",
+			"percentToQualify": 35,
+			"verificationVid": "https://youtu.be/PjbaJjptoTE",
+			"key": 1
+		},
+		/*=================================================================================*/
 	],
 	"version": [
 		1,
