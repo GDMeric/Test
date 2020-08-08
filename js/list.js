194bd87a8372ac941b6725799392a87656a09901
GDMeric <66123430+GDMeric@users.noreply.github.com>
2020-08-08 20:05:54 +0200

    Update list.js

--- js/list.js
+++ js/list.js
@@ -206,6 +206,12 @@ const d = {
 		/*=================================================================================*/
 		{
 			"vids": [
+				{
+                                         "user": "NotFluyd",
+                                         "link": "https://www.youtube.com/watch?v=Nc_OEOQn8jU&feature=youtu.be",
+                                         "percent": 42,
+                                         "hz": "144hz"
+                                },	
 			],
 			"name": "Circus Show",
 			"author": "Cailloux",
